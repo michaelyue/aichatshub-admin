f10ce47de5564c7289245656ecb3334e76eb9c7f
Michael <463329348@qq.com>
2025-01-12 22:20:36 +1100

    fix: save keysecret in pgoress

--- apps/web-antd/src/views/dashboard/workspace/index.vue
+++ apps/web-antd/src/views/dashboard/workspace/index.vue
@@ -11,7 +11,11 @@ import { preferences } from '@vben/preferences';
 import { useUserStore } from '@vben/stores';
 import { openWindow } from '@vben/utils';
 
+import { $t } from '#/locales';
+
 const userStore = useUserStore();
+
+const projectName = $t('page.dashboard.workspace');
 
 // 这是一个示例数据，实际项目中需要根据实际情况进行调整
 // url 也可以是内部路由，在 navTo 方法中识别处理，进行内部跳转
@@ -33,7 +37,7 @@ const router = useRouter();
 // 这是一个示例方法，实际项目中需要根据实际情况进行调整
 // This is a sample method, adjust according to the actual project requirements
 function navTo(nav: WorkbenchProjectItem | WorkbenchQuickNavItem) {
-  if (nav.url?.startsWith('http')) {
+  if (nav.url?.startsWith('http') || nav.url?.startsWith('https')) {
     openWindow(nav.url);
     return;
   }
@@ -60,7 +64,11 @@ function navTo(nav: WorkbenchProjectItem | WorkbenchQuickNavItem) {
 
     <div class="mt-5 flex flex-col lg:flex-row">
       <div class="mr-4 w-full lg:w-3/5">
-        <WorkbenchProject :items="projectItems" title="项目" @click="navTo" />
+        <WorkbenchProject
+          :items="projectItems"
+          :title="projectName"
+          @click="navTo"
+        />
         <!--  <WorkbenchTrends :items="trendItems" class="mt-5" title="最新动态" /> -->
       </div>
       <!--
--- apps/web-antd/src/views/settings/openai/index.vue
+++ apps/web-antd/src/views/settings/openai/index.vue
@@ -1,66 +1,51 @@
 <script lang="ts" setup>
 import { Page } from '@vben/common-ui';
+import { useUserStore } from '@vben/stores';
 
-import { Button, Card, message, notification, Space } from 'ant-design-vue';
+import { message } from 'ant-design-vue';
 
-type NotificationType = 'error' | 'info' | 'success' | 'warning';
+import { useVbenForm } from '#/adapter/form';
+import { $t } from '#/locales';
 
-function info() {
-  message.info('How many roads must a man walk down');
-}
+const userStore = useUserStore();
+const userId = userStore.userInfo?.userId || '';
 
-function error() {
-  message.error({
-    content: 'Once upon a time you dressed so fine',
-    duration: 2500,
-  });
-}
+const [BaseForm] = useVbenForm({
+  commonConfig: {
+    componentProps: {
+      class: 'w-full',
+    },
+  },
 
-function warning() {
-  message.warning('How many roads must a man walk down');
-}
-function success() {
-  message.success('Cause you walked hand in hand With another man in my place');
-}
+  handleSubmit: onSubmit,
+  layout: 'horizontal',
+  schema: [
+    {
+      component: 'Hidden',
+      fieldName: 'userId',
+      defaultValue: userId,
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: $t('settings.openai-key-secret-placehold'),
+      },
+      fieldName: 'secretkey',
+      label: $t('settings.openai-key-secret'),
+    },
+  ],
+  wrapperClass: 'grid-cols-1',
+});
 
-function notify(type: NotificationType) {
-  notification[type]({
-    duration: 2500,
-    message: '说点啥呢',
-    type,
+function onSubmit(values: Record<string, any>) {
+  message.success({
+    content: `form values: ${JSON.stringify(values)}`,
   });
 }
 </script>
 
 <template>
-  <Page
-    description="支持多语言，主题功能集成切换等"
-    title="Ant Design Vue组件使用演示"
-  >
-    <Card class="mb-5" title="按钮">
-      <Space>
-        <Button>Default</Button>
-        <Button type="primary"> Primary </Button>
-        <Button> Info </Button>
-        <Button danger> Error </Button>
-      </Space>
-    </Card>
-    <Card class="mb-5" title="Message">
-      <Space>
-        <Button @click="info"> 信息 </Button>
-        <Button danger @click="error"> 错误 </Button>
-        <Button @click="warning"> 警告 </Button>
-        <Button @click="success"> 成功 </Button>
-      </Space>
-    </Card>
-
-    <Card class="mb-5" title="Notification">
-      <Space>
-        <Button @click="notify('info')"> 信息 </Button>
-        <Button danger @click="notify('error')"> 错误 </Button>
-        <Button @click="notify('warning')"> 警告 </Button>
-        <Button @click="notify('success')"> 成功 </Button>
-      </Space>
-    </Card>
+  <Page :description="$t('settings.desc')" :title="$t('settings.title')">
+    <BaseForm />
   </Page>
 </template>
