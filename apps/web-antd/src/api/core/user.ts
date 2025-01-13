1d6ad7402bb9e0d7ee9040e681ae9cd99a370f96
Michael <463329348@qq.com>
2025-01-13 22:37:11 +1100

    fix: user api key added

--- apps/web-antd/src/api/core/user.ts
+++ apps/web-antd/src/api/core/user.ts
@@ -2,9 +2,17 @@ import type { UserInfo } from '@vben/types';
 
 import { requestClient } from '#/api/request';
 
+export interface UserAPIBundle {
+  keysecret: string;
+}
+
 /**
  * 获取用户信息
  */
 export async function getUserInfoApi() {
   return requestClient.get<UserInfo>('/user/info');
+}
+
+export async function getUserAPIKey() {
+  return requestClient.get<UserAPIBundle>('/user/key');
 }
--- apps/web-antd/src/views/settings/openai/index.vue
+++ apps/web-antd/src/views/settings/openai/index.vue
@@ -1,14 +1,21 @@
 <script lang="ts" setup>
+import type { UserAPIBundle } from '#/api';
+
+import { onMounted, ref } from 'vue';
+
 import { Page } from '@vben/common-ui';
 import { useUserStore } from '@vben/stores';
 
 import { message } from 'ant-design-vue';
 
 import { useVbenForm } from '#/adapter/form';
+import { getUserAPIKey } from '#/api';
 import { $t } from '#/locales';
 
 const userStore = useUserStore();
 const userId = userStore.userInfo?.userId || '';
+
+const keySecret = ref<string>('');
 
 const [BaseForm] = useVbenForm({
   commonConfig: {
@@ -30,8 +37,9 @@ const [BaseForm] = useVbenForm({
       componentProps: {
         placeholder: $t('settings.openai-key-secret-placehold'),
       },
-      fieldName: 'secretkey',
+      fieldName: 'keysecret',
       label: $t('settings.openai-key-secret'),
+      modelValue: keySecret,
     },
   ],
   wrapperClass: 'grid-cols-1',
@@ -42,10 +50,21 @@ function onSubmit(values: Record<string, any>) {
     content: `form values: ${JSON.stringify(values)}`,
   });
 }
+
+async function fetchUserKey() {
+  let userKey: null | UserAPIBundle = null;
+  userKey = await getUserAPIKey();
+  keySecret.value = userKey === null ? '' : userKey.keysecret;
+}
+
+// Fetch the user key when component is mounted
+onMounted(async () => {
+  await fetchUserKey();
+});
 </script>
 
 <template>
   <Page :description="$t('settings.desc')" :title="$t('settings.title')">
-    <BaseForm />
+    <BaseForm v-model:keysecret="keySecret" />
   </Page>
 </template>
